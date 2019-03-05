336ac32af9e0e940942d9ea313adff89a2fba58e
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2019-03-05 21:37:57 +0000

    Change organisation query functioning

--- pages/organisation/index.vue
+++ pages/organisation/index.vue
@@ -1,20 +1,19 @@
 <template>
   <section id="organisation">
-
-    <section v-if="organisation.name">
+    <section v-if="organisation">
       <hero-nav
-        :title="organisation.name"
-        description="Manage your organisation details, levels, teams, positions and users"
-        :heroNavItems="heroNavItems"
+      :title="organisation.name"
+      description="Manage your organisation details, levels, teams, positions and users"
+      :heroNavItems="heroNavItems"
       >
-      </hero-nav>
-    </section>
-
-    <section>
-      <NuxtChild :organisation="organisation" />
-    </section>
-
+    </hero-nav>
   </section>
+
+  <section v-if="organisation">
+    <NuxtChild :organisation="organisation" />
+  </section>
+
+</section>
 </template>
 
 <style lang="scss">
@@ -23,84 +22,22 @@
 
 <script>
 import HeroNav from "~/components/general/HeroNav";
-import gql from "graphql-tag";
-import { error } from "util";
-
-const orgQuery = gql`
-query($email: String!){
-  user(email:$email){
-    firstName,
-    organisationSet{
-      name,
-      website,
-      branches,
-      cfFrequency,
-      levelSet{
-        label,
-        number,
-        teamSet{
-          id
-          name,
-          parent{
-            id
-          },
-          active,
-          positionSet{
-            id,
-            title,
-            userpositionSet{
-              user{
-                id,
-                firstName,
-                lastName,
-                email
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-`;
 
 export default {
   layout: "default",
   components: {
     HeroNav
   },
+  asyncData(context){
+    //initialize
+    if(context.route.query.login){
+      context.store.dispatch('nuxtClientInit',context)
+      context.app.router.replace('/organisation')
+    }
+  },
   computed:{
-    organisation:function() {
-
-      if(typeof this.$store.state.user.user.username == 'undefined'){
-        return {};
-      }
-
-      if(typeof this.$store.state.organisation.organisation.name == 'undefined'){
-        this.$toast.show('Loading organisation info...')
-        // but you could also call queries like this:
-        return this.$apollo.query({ query: orgQuery, variables: {email:this.$store.state.user.user.email} })
-        .then(({ data }) => {
-          //save organisation details
-          const organisation = data.user.organisationSet[0]
-          this.$store.commit('organisation/setOrg', {
-            name: organisation.name,
-            website:organisation.website,
-            branches:organisation.branches,
-            cfFrequency:organisation.cfFrequency,
-            created:organisation.created,
-            levelSet:organisation.levelSet,
-          })
-          this.$toast.success('Organisation info loaded')
-          return organisation;
-        })
-        .catch(err => {
-          console.log(err)
-          this.$toast.error('An error occured, please try again.')
-        });
-      }else{
-        return this.$store.state.organisation.organisation;
-      }
+    organisation(){
+      return this.$store.getters['organisation/ORGANISATION']
     }
   },
   data() {
@@ -119,7 +56,7 @@ export default {
       if(path === target){
         return 'is-active'
       }
-    },
+    }
   }
 };
 </script>
