9a6a848fbf1ccf74497b47862c52026614bcc4dd
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2019-03-10 20:09:20 +0000

    Build

--- dist/_nuxt/d6de38ba5f24471bddd5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{885:function(e,n,t){"use strict";t.r(n);var l=t(29),component=Object(l.a)({},function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("View")])},[],!1,null,null,null);n.default=component.exports}}]);
